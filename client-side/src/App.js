58c5dd2a6c16743a2468be30a9d73a6e2fb0fe59
215295 <sumonasad04717@gmail.com>
2022-11-27 14:39:00 +0600

    first commit

---
+++ client-side/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="text-3xl font-bold underline">
+      hello everyone...
+    </h1>
+    <p>I am a loser</p>
+    </div>
+  );
+}
+
+export default App;
