7d9e85710b0d1c21e8db4a464f2545602a7b96d5
215295 <sumonasad04717@gmail.com>
2022-11-30 19:22:43 +0600

    added

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,17 +1,17 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes/Routes';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className="text-3xl font-bold underline">
-      hello everyone...
-    </h1>
-    <p>I am a loser</p>
+    <div className='max-w-[1440px] mx-auto'>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
 
-export default App
+export default App;
+
 //npm i react-router-dom react-hot-toast react-hook-form
 //npm i  react-day-picker date-fns firebase daisyui  
